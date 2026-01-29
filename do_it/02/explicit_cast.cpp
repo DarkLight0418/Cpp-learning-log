ffd5ec7ebaf8e46ab61c656bf4c5bf5494ab089e
DarkLight0418 <samseong0418@naver.com>
2026-01-29 11:34:13 +0900

    20260129 주석 빠뜨려서 재수정

--- do_it/02/explicit_cast.cpp
+++ do_it/02/explicit_cast.cpp
@@ -9,6 +9,9 @@ int main()
   int int_avg = (int_a + int_b) / 2;
   float float_avg_1 = (int_a + int_b) / 2;
   float float_avg_2 = float(int_a + int_b) / 2;
+  /*
+    float_avg_1 : 그냥 사용한 곳(값이 달라짐), float_avg_2 : 명시적 형 변환 사용한 곳(값 유지)
+  */
 
   cout << "int_avg: " << int_avg << endl;
   cout << "float_avg_1: " << float_avg_1 << endl;

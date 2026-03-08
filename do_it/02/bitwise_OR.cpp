9173888b51e286feb4a973195da3be91fbb15617
DarkLight0418 <samseong0418@naver.com>
2026-03-08 20:38:16 +0900

    [study, remove] 2026-03-08: 비트 OR, gitignore 추가, c-cpp-compile-run gcc 설정 다시 봐야 함

--- do_it/02/bitwise_OR.cpp
+++ do_it/02/bitwise_OR.cpp
@@ -5,11 +5,11 @@ int main()
 {
   int a = 13;
   int b = 27;
-  int c = a & b; // 비트 AND
+  int c = a | b; // 비트 OR
 
   std::cout << "a = " << std::bitset<8>(a) << " : " << a << std::endl; // 13
   std::cout << "b = " << std::bitset<8>(b) << " : " << b << std::endl; // 27
-  std::cout << "c = " << std::bitset<8>(c) << " : " << c << std::endl; // 9
+  std::cout << "c = " << std::bitset<8>(c) << " : " << c << std::endl; // 31
 
   return 0;
 }
